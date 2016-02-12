33db772464e8011c30943d853599f7addddca37b
preston.sbcoba <sbcoba@gmail.com>
2016-02-13 03:28:31 +0900

    AccessToken 테스트를 위한 코드 추가와 로그 설정 변경

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,7 +28,15 @@ import java.io.Serializable;
 @EnableResourceServer
 @EnableAuthorizationServer
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/authorization-code-test").access("#oauth2.hasScope('read')")
+			.antMatchers("/members").access("#oauth2.hasScope('read')");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -42,5 +59,26 @@ class Member implements Serializable {
 		this.name = name;
 		this.username = username;
 		this.remark = remark;
+	}
+}
+
+/**
+ * 권한 코드 테스트를 위해 만든 컨트롤러
+ */
+@Controller
+@RequestMapping("test")
+class TestController {
+	@RequestMapping("authorization-code")
+	@ResponseBody
+	public String authorizationCodeTest(@RequestParam("code") String code) {
+		String curl = String.format("curl " +
+				"-F \"grant_type=authorization_code\" " +
+				"-F \"code=%s\" " +
+				"-F \"scope=read\" " +
+				"-F \"client_id=foo\" " +
+				"-F \"client_secret=bar\" " +
+				"-F \"redirect_uri=http://localhost:8080/test/authorization-code\" " +
+				"\"http://foo:bar@localhost:8080/oauth/token\"", code);
+		return curl;
 	}
 }
