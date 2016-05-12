9c88959b76526d2add5b2210adabb6eb254efcd1
Lee su hong <sbcoba@gmail.com>
2016-05-12 23:56:31 +0900

    메소드명 변경

--- oauth2-server/src/main/java/com/example/OAuth2Application.java
+++ oauth2-server/src/main/java/com/example/OAuth2Application.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -31,39 +27,17 @@ class JwtOAuth2AuthorizationServerConfiguration extends OAuth2AuthorizationServe
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
 			throws Exception {
 		super.configure(endpoints);
-		endpoints.accessTokenConverter(jwtTokenEnhancer());
+		endpoints.accessTokenConverter(jwtAccessTokenConverter());
 	}
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new JwtTokenStore(jwtTokenEnhancer());
+		return new JwtTokenStore(jwtAccessTokenConverter());
 	}
 
 	@Bean
-	public JwtAccessTokenConverter jwtTokenEnhancer() {
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		return new JwtAccessTokenConverter();
-	}
-
-}
-@Configuration
-class CustomOAuth2AuthorizationServerConfiguration extends OAuth2AuthorizationServerConfiguration {
-
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-			throws Exception {
-		super.configure(endpoints);
-		endpoints.tokenServices(new DefaultTokenServices());
-	}
-
-	@Bean
-	public DefaultTokenServices defaultTokenServices() {
-		return new DefaultTokenServices() {
-			@Override
-			public OAuth2Authentication loadAuthentication(String accessTokenValue) throws AuthenticationException,
-					InvalidTokenException {
-				return super.loadAuthentication(accessTokenValue);
-			}
-		};
 	}
 
 }
