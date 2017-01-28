180f51472eef2f030981914b5c00e4c43b587634
sbcoba <sbcoba@gmail.com>
2017-01-28 15:03:10 +0900

    스프링 부트 버전 업, Deprecated 관련 코드 제거, 버전에 맞도록 설정 변경

--- oauth2-server/src/main/java/com/example/OAuth2Application.java
+++ oauth2-server/src/main/java/com/example/OAuth2Application.java
@@ -1,12 +1,16 @@
 package com.example;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -15,29 +19,44 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @SpringBootApplication
 public class OAuth2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OAuth2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2Application.class, args);
+    }
+}
+
+@Configuration
+class OAuth2Configuration {
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        return new JwtAccessTokenConverter();
+    }
+
 }
 
 @Configuration
 class JwtOAuth2AuthorizationServerConfiguration extends OAuth2AuthorizationServerConfiguration {
 
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-			throws Exception {
-		super.configure(endpoints);
-		endpoints.accessTokenConverter(jwtAccessTokenConverter());
-	}
+    private final JwtAccessTokenConverter jwtAccessTokenConverter;
 
-	@Bean
-	public TokenStore tokenStore() {
-		return new JwtTokenStore(jwtAccessTokenConverter());
-	}
+    public JwtOAuth2AuthorizationServerConfiguration(BaseClientDetails details,
+                                                     AuthenticationManager authenticationManager,
+                                                     ObjectProvider<TokenStore> tokenStoreProvider,
+                                                     AuthorizationServerProperties properties,
+                                                     JwtAccessTokenConverter jwtAccessTokenConverter) {
+        super(details, authenticationManager, tokenStoreProvider, properties);
+        this.jwtAccessTokenConverter = jwtAccessTokenConverter;
+    }
 
-	@Bean
-	public JwtAccessTokenConverter jwtAccessTokenConverter() {
-		return new JwtAccessTokenConverter();
-	}
-
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+            throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(jwtAccessTokenConverter);
+    }
 }
--- oauth2-server/src/test/java/com/example/OAuth2ApplicationTests.java
+++ oauth2-server/src/test/java/com/example/OAuth2ApplicationTests.java
@@ -2,11 +2,11 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = OAuth2Application.class)
+@SpringBootTest(classes = OAuth2Application.class)
 public class OAuth2ApplicationTests {
 
 	@Test
